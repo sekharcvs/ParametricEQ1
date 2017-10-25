3dea670b1ae3685ad74708831550db84eb680d28
sekharcvs <cvs.sekhar@gmail.co>
2017-10-24 22:52:33 -0700

    Cleaned up code

--- Debug/cmds.bat
+++ Debug/cmds.bat
@@ -1,2 +1,2 @@
-SET INP=C:\Users\venkatac\Desktop\DATA15_COPY\codes\Stereo\Stereo_post_MPEGH\database\dynevsMono.pcm
-ParametricEQ.exe %INP% out.pcm eqGains0.pcm 16000
+SET INP=.\test_input.pcm
+ParametricEQ.exe %INP% out.pcm eqGains.pcm 48000
